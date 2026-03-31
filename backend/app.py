c4c1a052688774f15a34d7926de4bdcaf09e583f
Biswajit <biswajitkr.dandapat@gmail.com>
2026-03-31 18:11:49 +0530

    backend fix for render

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
